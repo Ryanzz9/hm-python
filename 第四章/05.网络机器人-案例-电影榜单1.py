db360fcf38dac370d00a80ef82e730185b81ffb4
Rzzz <xinxincn0506@outlook.com>
2026-09-02 16:39:06 +0800

    2026-9-02-1034 网络机器人-保存电影数据到CSV文件

--- 第四章/05.网络机器人-案例-电影榜单1.py
+++ 第四章/05.网络机器人-案例-电影榜单1.py
@@ -2,15 +2,17 @@ import requests
 import csv
 from lxml import html
 
+
 # 常量
 TMDB_BASE_URL = "https://www.themoviedb.org"
 TMDB_TOP_URL = "https://www.themoviedb.org/movie/top-rated"
-
+MOVIE_LIST_FILE = "csv_data/movie_list.csv"
 
 # 获取电影详情
 def get_movie_info(movie_info_url):
     # 1. 发送请求，获取电影详情数据
     movie_response = requests.get(movie_info_url,timeout=60)
+    print(f"发送请求{movie_info_url}，获取电影详情数据...")
 
     # 2.解析数据，获取电影详情
     movie_doc = html.fromstring(movie_response.text)
@@ -22,8 +24,8 @@ def get_movie_info(movie_info_url):
     movie_tags = movie_doc.xpath("//*[@id='original_header']/div[2]/section/div[1]/div/span[3]/a/text()")  # 类型
     movie_cost_times = movie_doc.xpath("//*[@id='original_header']/div[2]/section/div[1]/div/span[4]/text()")  # 时长
     movie_scores = movie_doc.xpath("//*[@id='consensus_pill']/div/div[1]/div/div/@data-percent")  # 评分
-    movie_languages = movie_doc.xpath(
-        "//*[@id='media_v4']/div/div[2]/div/section/div[1]/div/section[1]/p[3]/text()")  # 语言
+    #movie_languages = movie_doc.xpath("//*[@id='media_v4']/div/div[2]/div/section/div[1]/div/section[1]/p[3]/text()")
+    movie_languages = movie_doc.xpath("//*[@id='media_v4']/div/div/div[2]/div/section/div[1]/div/section[1]/p[3]/text()") #语言
     movie_directors = movie_doc.xpath("//*[@id='original_header']/div[2]/section/div[3]/ol/li[1]/p[1]/a/text()")  # 导演
     movie_authors = movie_doc.xpath("//*[@id='original_header']/div[2]/section/div[3]/ol/li[2]/p[1]/a/text()")  # 作者
     movie_slogans = movie_doc.xpath("//*[@id='original_header']/div[2]/section/div[3]/h3[1]/text()")  # 宣传语
@@ -35,24 +37,27 @@ def get_movie_info(movie_info_url):
 
     moive_info = {
         "电影名": movie_names[0].strip() if movie_names else '',
-        "电影年份": movie_years[0].strip() if movie_years else '',
-        "电影日期": movie_dates[0].strip() if movie_dates else '',
-        "电影标签": ",".join(movie_tags) if movie_tags else '',
-        "电影时长": movie_cost_times[0].strip() if movie_cost_times else '',
-        "电影评分": movie_scores[0].strip() if movie_scores else '',
-        "电影语言": movie_languages[0].strip() if movie_languages else '',
-        "电影导演": movie_directors[0].strip() if movie_directors else '',
-        "电影作者": movie_authors[0].strip() if movie_authors else '',
-        "电影宣传语": movie_slogans[0].strip() if movie_slogans else '',
-        "电影简介": movie_descriptions[0].strip() if movie_descriptions else ''
+        "年份": movie_years[0].strip() if movie_years else '',
+        "上映日期": movie_dates[0].strip() if movie_dates else '',
+        "标签": ",".join(movie_tags) if movie_tags else '',
+        "时长": movie_cost_times[0].strip() if movie_cost_times else '',
+        "评分": movie_scores[0].strip() if movie_scores else '',
+        "语言": movie_languages[0].strip() if movie_languages else '',
+        "导演": movie_directors[0].strip() if movie_directors else '',
+        "作者": movie_authors[0].strip() if movie_authors else '',
+        "宣传语": movie_slogans[0].strip() if movie_slogans else '',
+        "简介": movie_descriptions[0].strip() if movie_descriptions else ''
     }
-    print(moive_info)
+    #print(moive_info)
     return moive_info
 
 
 # 保存所有电影
 def save_all_movies(all_movies):
-    pass
+    with open(MOVIE_LIST_FILE, mode='w', newline='', encoding='utf-8') as csvfile:
+        writer_csv = csv.DictWriter(csvfile, fieldnames=["电影名", "年份", "上映日期", "标签", "时长", "评分", "语言", "导演", "作者", "宣传语", "简介"])
+        writer_csv.writeheader() # 写入表头
+        writer_csv.writerows(all_movies) # 写入数据
 
 
 #主函数，定义核心逻辑
@@ -61,8 +66,10 @@ def save_all_movies(all_movies):
 def main():
     # 1.发送请求，获取高分电影榜单数据
     response = requests.get(TMDB_TOP_URL, timeout=60)
+    print("发送请求，获取TMDB电影榜单数据...")
     print("状态码：", response.status_code)          # 看看是否是 200
     print("页面长度：", len(response.text))          # 正常应该在几十KB以上
+
 
     #2.解析数据，获取电影列表
     document = html.fromstring(response.text)
@@ -88,9 +95,10 @@ def main():
            # 发送请求，获取电影详情数据
            movie_info = get_movie_info(movie_info_url)
            all_movies.append(movie_info)
-           print(all_movies)
+           #print(all_movies)
 
     # 4.保存数据，保存为 csv文件
+    print("获取到所有的电影详情，保存电影数据到CSV文件...")
     save_all_movies(all_movies)
 
 if __name__ == '__main__':
