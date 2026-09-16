3e2ced8bc9435ba23a278ab56cf1171621f29b7e
Rzzz <xinxincn0506@outlook.com>
2026-09-16 10:54:09 +0800

    2026-9-16-1054 新建会话功能实现

--- 第六章/汉字谜盒/main.py
+++ 第六章/汉字谜盒/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+from pip._internal.utils import datetime
 from starlette.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import os
+from datetime import datetime
+import json
+from pydantic import BaseModel
+from typing import Any
+from openai import OpenAI
+
+
+# 生成会话的标识
+def generate_session_id():
+    return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+
+class ApiResponse(BaseModel):
+    code: int
+    message: str
+    data: Any # 任意类型的数据
+
 
 # 创建FastAPI应用
 app = FastAPI(title="汉字谜盒")
@@ -9,6 +28,11 @@ app = FastAPI(title="汉字谜盒")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+#sessions
+
+if not os.path.exists("sessions"):
+    os.mkdir("sessions")
+
 
 # 定义根路径操作
 @app.get("/")
@@ -16,7 +40,24 @@ def root():
     print("访问项目首页")
     return FileResponse("static/index.html")
 
+# 创建会话
+@app.post("/api/sessions")
+def create_session() ->ApiResponse:
+    print("创建会话")
+    # 1. 生成会话的标识(名字)
+    session_id = generate_session_id()
 
+    # 2.组装会话信息，保存到文件
+    session_data = {
+        "current_session": session_id,
+        "messages": []
+    }
+
+    with open(f"sessions/{session_id}.json", "w", encoding="utf-8") as f:
+        json.dump(session_data, f, ensure_ascii=False, indent=2)
+
+    # 3. 返回数据
+    return ApiResponse(code=0, message="创建会话成功", data={"session_id": session_id})
 
 
 # 运行应用
